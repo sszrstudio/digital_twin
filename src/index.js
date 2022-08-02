91e732ef14c9c429aa6332bf0c97683c699cd828
sunyan <854591328@qq.com>
2022-08-02 22:38:14 +0800

    Baoshijie

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+911.glb
+Author: Karol Miklas https://sketchfab.com/karolmiklas
+Sketchfab: https://sketchfab.com/3d-models/free-porsche-911-carrera-4s-d01b254483794de3819786d93e0e1ebf */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import { render } from 'react-dom'
+import { Suspense } from 'react'
+import { Footer } from '@pmndrs/branding'
+import './styles.css'
+import { App } from './App'
+
+render(
+  <Suspense fallback={null}>
     <App />
-  </React.StrictMode>
-);
+    <Overlay />
+  </Suspense>,
+  document.getElementById('root')
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Overlay() {
+  return (
+    <>
+      <Footer
+        date="06. March"
+        year="2022"
+        link1={<a href="https://github.com/pmndrs/drei">pmndrs/drei</a>}
+        link2={<a href="https://codesandbox.io/s/building-live-envmaps-lwo219">s/lwo219</a>}
+      />
+      <div style={{ color: '#808080', position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>neutral —</div>
+    </>
+  )
+}
